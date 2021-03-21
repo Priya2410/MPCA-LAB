0ab43f5a3cf0d5a5f4daff8be280f7a5fa13ec52
Priya Mohata <56394628+Priya2410@users.noreply.github.com>
2021-03-21 18:47:10 +0530

    Delete WEEK-1 directory

--- WEEK-1/Programs/program4-testcase1_PES2UG19CS301.s
+++
@@ -1,9 +0,0 @@
-.text
-MOV r1,#6
-ANDS r2,r1,#1 
-BEQ label
-MOV r0,#255
-B exit
-label:MOV r0,#0
-exit:SWI 0x11
-.end
--- WEEK-1/Programs/program4-testcase2_PES2UG19CS301.s
+++
@@ -1,9 +0,0 @@
-.text
-MOV r1,#5
-ANDS r2,r1,#1 @To update the flags of CPSR register
-BEQ label
-MOV r0,#255
-B exit
-label:MOV r0,#0
-exit:SWI 0x11
-.end
